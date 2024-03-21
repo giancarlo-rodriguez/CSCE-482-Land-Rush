45bd9a8e9fcf52caca628608370a745d4c208f5f
Sasan Lotfi <sasan1380.birjand@gmail.com>
2024-03-21 13:12:02 -0500

    Permissions

---
+++ landrush/app/permissions.py
@@ -0,0 +1,22 @@
+from rest_framework import permissions
+from .models import Role, Organization
+
+class IsUniversity(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return(request.user.is_university)
+
+class IsStudent(permissions.BasePermission):
+    def has_permission(self,request,view):
+        return(not request.user.is_university)
+
+class IsOrgAdmin(permissions.BasePermission):
+    def has_permission(self,request,view):
+        org_id = request.GET["organization"]
+        org = Organization.objects.get(id=org_id)
+        org_admins = Role.objects.get(organization = org, is_admin = True)
+        for org_admin in org_admins:
+            if(org_admin.user == request.user):
+                return True
+        return False
+
+
