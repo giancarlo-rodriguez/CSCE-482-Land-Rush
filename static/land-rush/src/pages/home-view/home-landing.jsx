57f32c1af269aea90ff82a53c5ada1b9b19ae96f
Gian Rodriguez <gicaro3@tamu.edu>
2024-04-28 04:22:50 -0500

    Added text to home page

--- static/land-rush/src/pages/home-view/home-landing.jsx
+++ static/land-rush/src/pages/home-view/home-landing.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 function HomeLanding() {
   return (
-    <div className="container">
-      <h1>Put Something here</h1>
+    <div className="container" style={{ textAlign: 'center' }}>
+      <h1>OUR MISSION:</h1>
+      <h1>“Fair allocation of land with human comfort and accessibility in mind”</h1>
+      <br />
+      <h1>WHY?</h1>
+      <h1>The current system for student organizations and their members to select an area for tailgating is outdated, inefficient, and inaccessible by many</h1>
+      <br />
+      <h1>HOW?</h1>
+      <h1>Via a full-stack application, designed to streamline university event registration for student organizations and their members</h1>
     </div>
   );
 }
