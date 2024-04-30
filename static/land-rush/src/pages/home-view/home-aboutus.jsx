29056bd321f1c1509496d9512dd791702f9b61a1
Chandler S. <112497664+mistachan@users.noreply.github.com>
2024-04-30 11:19:30 -0500

    Landing Progress
    Updated Landing css for homepage content and instructions, additionally removed useless/joke objects from the database.

--- static/land-rush/src/pages/home-view/home-aboutus.jsx
+++ static/land-rush/src/pages/home-view/home-aboutus.jsx
@@ -5,13 +5,22 @@ import document from '../../content/Land_Rush.pdf'
 function HomeAboutus() {
   return (
     <div className="container">
+      <div className='instructions-container'>
+      <div className='instructions'>
+        <h1>OUR MISSION:</h1>
+        <h2>“Fair allocation of land with human comfort and accessibility in mind.”</h2>
+        <br />
+        <h1>WHY?</h1>
+        <h2>The current system for student organizations and their members to take part in the tailgating process on-campus is outdated, inefficient, and inaccessible by many.</h2>
+        <br />
+        <h1>OUR SOLUTION</h1>
+        <h2>Land Rush seeks to streamline the process of creating and rushing events through a more fair and organized process which grants equal opportunity to all who participate.</h2>
+      </div>
+    </div>
       <div style={{ textAlign: 'center' }}>
         <pre style={{ fontSize: '300%', maxWidth: '100%', overflow: 'hidden', textDecoration: 'underline', fontWeight: 'bold' }}>
           Land Rush Overview and Documentation
         </pre>
-      </div>
-      <div>
-        <img src={img} alt="Land Rush Poster" style={{ width: '100%' }} />
       </div>
       <div style={{ textAlign: 'center' }}>
         <embed src={document} type="application/pdf" width="100%" height="1120px" />
--- static/land-rush/src/pages/home-view/home-landing.jsx
+++ static/land-rush/src/pages/home-view/home-landing.jsx
@@ -2,15 +2,37 @@ import React from 'react';
 
 function HomeLanding() {
   return (
-    <div className="container" style={{ textAlign: 'center' }}>
-      <h1>OUR MISSION:</h1>
-      <h1>“Fair allocation of land with human comfort and accessibility in mind”</h1>
-      <br />
-      <h1>WHY?</h1>
-      <h1>The current system for student organizations and their members to select an area for tailgating is outdated, inefficient, and inaccessible by many</h1>
-      <br />
-      <h1>HOW?</h1>
-      <h1>Via a full-stack application, designed to streamline university event registration for student organizations and their members</h1>
+    <div className='instructions-container'>
+      <div className='instructions'>
+        <h1>Welcome to Land Rush!</h1>
+        <h4>A web-application developed by Chandler Stanchfield, Wyatt Griffin, Gian Rodriguez, Sasan Lotfi, and Justin Kline.</h4>
+      </div>
+      <div className='instructions'>
+        <h1>Our Application</h1>
+        <h3>This site demonstrates the primary use-case for our dynamic land-allocation algorithm, tailgating.</h3>
+        <h3>Through the app, universities may sign up, designate land plots, and create timed events.</h3>
+        <h3>Students can register with their universities and create organizations.</h3>
+        <h3> Organizations and their members may register for events at certain times.</h3>
+        <h3>These registration times are averaged for each organization when registration closes to create a queue, which is then sent to our algorithm to designate 
+          sections of land, maximizing space efficiency using a minimum requirement of seven square feet per person; a minimum determined based on human-comfort and 
+          safety analysis conducted prior to development.</h3> 
+        <h3>For a more detailed breakdown of this algorithm, please visit our About Us section in the navigation bar to view our design document in full detail, which
+          breaks down the full development methodology, process, and our findings.</h3>
+      </div>
+      <div className='instructions'>
+        <h1>Getting Started</h1>
+        <p>
+          <strong>Students and Organization Leaders:</strong> Register as a student via the Register Student link in the top navigation bar.
+        </p>
+        <p>
+          <strong>Universities:</strong> Register as a new college or university via the Register University link in the top navigation bar. 
+        </p>
+        <p>
+          <strong>Existing Users:</strong> Register as a new college or university via the Register University link in the top navigation bar. 
+        </p>
+        <p>
+        </p>
+      </div>
     </div>
   );
 }
